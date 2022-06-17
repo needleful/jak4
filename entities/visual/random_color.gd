8bad2eec84fd9d47dda6e0ef3db8d230cb0e0b36
needleful <needleful@outlook.com>
2022-06-16 20:32:44 -0700

    Electrical props

--- entities/visual/random_color.gd
+++ entities/visual/random_color.gd
@@ -9,8 +9,10 @@ onready var mesh = get_node(mesh_instance)
 func _ready():
 	var color := palette.interpolate(randf())
 	
-	var mat: ShaderMaterial = mesh.get_surface_material(0)
-	if mat:
+	var mat = mesh.get_surface_material(0)
+	if mat is ShaderMaterial:
 		mat.set_shader_param(shader_param, color)
+	elif mat is SpatialMaterial:
+		mat.albedo_color = color
 	else:
-		print_debug("No shader material for mesh: ", mesh.get_path())
+		print_debug("No material for mesh: ", mesh.get_path())
