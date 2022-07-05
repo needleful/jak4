b7cc16a8521f9eca7142bb5d103464ef0315cb4b
needleful <needleful@outlook.com>
2022-07-04 22:21:04 -0700

    Improved trail effect

--- addons/np_trail/trail.gd
+++ addons/np_trail/trail.gd
@@ -6,6 +6,7 @@ export(float) var width := 0.03
 export(Vector3) var starting_velocity := Vector3.ZERO
 export(Vector3) var random_velocity := Vector3.ZERO
 export(Gradient) var color: Gradient
+export(float) var cull_distance := 10.0
 
 var position: PoolVector3Array = []
 var velocity: PoolVector3Array = []
@@ -22,25 +23,41 @@ func _process(delta):
 	for i in position.size():
 		position[i] += velocity[i]*delta
 	
-	var c_pos := get_viewport().get_camera().global_transform.origin
+	var camb: Basis = get_viewport().get_camera().global_transform.basis
 	
 	clear()
 	begin(Mesh.PRIMITIVE_TRIANGLES)
 	for i in position.size() - 1:
-		var g_pos := global_transform*position[i]
-		var g_next := global_transform*position[i+1]
+		var g_pos := position[i]
+		var g_next := position[i+1]
 		var dir := g_next - g_pos
-		var cam_dir := (c_pos - g_pos)
-		var third = dir.cross(cam_dir).normalized()*width*(segments-i)/segments
+		var prev := Vector3.ZERO
+		if i > 0:
+			prev = g_pos - position[i-1]
+		var nprev := prev.slide(camb.z).normalized()
+		var ndir := dir.slide(camb.z).normalized()
+		var third: Vector3 = (ndir - nprev).normalized()*width*(segments-i)/segments
+		if third.dot(camb.x) < 0:
+			third = -third
+		var fourth := third
+		if i < position.size() - 2:
+			var next := position[i+2] - g_next
+			var nnext = next.slide(camb.z).normalized()
+			fourth = (nnext - ndir).normalized()*width*(segments-1-i)/segments
+			if fourth.dot(camb.x) < 0:
+				fourth = -fourth
+		
+		var point1 = global_transform.xform_inv(g_pos)
+		var point2 = global_transform.xform_inv(g_next)
 		var point3 = global_transform.xform_inv(g_pos + third)
-		var point4 = global_transform.xform_inv(g_next + third)
+		var point4 = global_transform.xform_inv(g_next + fourth)
 		
-		add_vertex(position[i])
-		add_vertex(position[i+1])
+		add_vertex(point1)
+		add_vertex(point2)
 		add_vertex(point3)
 		
 		add_vertex(point3)
-		add_vertex(position[i+1])
+		add_vertex(point2)
 		add_vertex(point4)
 	end()
 
@@ -54,5 +71,16 @@ func add_point():
 		velocity[0] = starting_velocity + 2*random_velocity*Vector3(
 				randf(), randf(), randf()
 			) - random_velocity
-	position.insert(0, Vector3.ZERO)
+		velocity[0] = global_transform.basis*velocity[0]
+	position.insert(0, global_transform.origin)
 	velocity.insert(0, Vector3.ZERO)
+
+func process_player_distance(point):
+	if (point - global_transform.origin).length_squared() > cull_distance*cull_distance:
+		if visible:
+			set_process(false)
+			hide()
+	elif !visible:
+		set_process(true)
+		show()
+	return INF
