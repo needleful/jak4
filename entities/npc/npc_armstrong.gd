74a1f9aa3dc7692905132822dace3286215073de
needleful <needleful@outlook.com>
2022-09-12 20:10:35 -0700

    Fixing more prompts

--- entities/npc/npc_armstrong.gd
+++ entities/npc/npc_armstrong.gd
@@ -9,6 +9,10 @@ func start_pistol_tutorial():
 	start_wep_tutorial("pistol")
 
 func start_wep_tutorial(wep: String):
+	if wep == "pistol":
+		$"../tutorials/pistol/prompt_aim".input_actions.append(
+			"combat_aim_toggle" if Global.using_gamepad else "combat_aim")
+
 	tutorial = wep
 	var tw = tut.get_node(tutorial)
 	global_transform = tw.get_node("ref_armstrong").global_transform
