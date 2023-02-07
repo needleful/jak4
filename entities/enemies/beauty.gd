377376118a8d3dbca41c58372ea6fef74cbd554b
needleful <needleful@outlook.com>
2023-02-06 22:10:26 -0800

    Got the guy jumping

--- entities/enemies/beauty.gd
+++ entities/enemies/beauty.gd
@@ -36,7 +36,7 @@ func _ready():
 	if p is PlayerBody:
 		player = p
 		player_last_origin = player.global_transform.origin
-		calculate_path(player_last_origin)
+		var _x = calculate_path(player_last_origin)
 
 func _physics_process(delta):
 	if !player:
@@ -49,27 +49,41 @@ func _physics_process(delta):
 		next_pos = calculate_path(loc)
 	else:
 		next_pos = nav.get_next_location()
-	rotate_toward(next_pos, delta)
 	var dir := (next_pos - global_transform.origin).normalized()
 	var final_pos:Vector3 = nav.get_final_location()
 	var final_diff := final_pos - global_transform.origin
-	if move_state == MoveState.Jumping:
+	
+	if move_state == MoveState.JumpCharge:
+		velocity.y = get_jump_velocity(player.global_transform.origin - global_transform.origin)
+		move_state = MoveState.Jumping
+	elif move_state == MoveState.Jumping:
 		dir = (player.global_transform.origin - global_transform.origin).normalized()
-		air_move(delta, dir)
+		air_move(delta, dir*2, 12.0)
 		if is_on_floor():
 			move_state = MoveState.Ground
-	elif final_diff.length() > 2:
+			var _x = calculate_path(loc)
+	else:
+		if final_diff.length() < 2:
+			dir = Vector3.ZERO
+			if !nav.is_target_reachable():
+				move_state = MoveState.JumpCharge
 		if is_on_floor():
 			ground_move(delta, dir)
 		else:
 			air_move(delta, dir)
-	elif !nav.is_target_reachable():
-		velocity.y = get_jump_velocity(player.global_transform.origin - global_transform.origin)
-		move_state = MoveState.Jumping
+	
+	rotate_toward(dir, delta)
 
-func get_jump_velocity(difference: Vector3):
-	var diff_y := difference.y
-	return 9.0
+func get_jump_velocity(difference: Vector3) -> float:
+	var a := abs(GRAVITY.y)
+	var p := max(difference.y, 0.0) + 2.0
+
+	# Air time at peak = v0/a
+	# Height at a point in time = 0.5at^2 + v0t
+	# Height at peak = 1.5*(v0^2/a)
+	# Velocity to reach peak p = sqrt(2/3ap)
+	var v0 := sqrt(2 * a * p)
+	return v0
 
 func ground_move(delta: float, dir: Vector3):
 	var gravity: Vector3
@@ -86,26 +100,25 @@ func ground_move(delta: float, dir: Vector3):
 	velocity = move_and_slide(
 		velocity, 
 		#Vector3.DOWN*0.25,
-		Vector3.UP, false, 4, 200.0)
+		Vector3.UP, false, 4, 0.5*PI)
 
-func air_move(delta:float, dir: Vector3):
+func air_move(delta:float, dir: Vector3, accel_scale := 1.0):
 	dir.y = 0
-	var movement := 4.0*dir.normalized()
+	var movement := 4.0*dir
 	var vy := velocity.y
 	velocity.y = 0
-	velocity = velocity.move_toward(movement, 5.0*delta)
-	velocity.y += vy
+	velocity = velocity.move_toward(movement, accel_scale*5.0*delta)
+	velocity.y = vy
 	velocity += delta*GRAVITY
 	velocity = move_and_slide(velocity, Vector3.UP, false, 4, 1.7)
 
-func rotate_toward(point:Vector3, delta:float):
+func rotate_toward(dir:Vector3, delta:float):
 	# TODO: change forward when we get a real mesh
 	var forward := -body.global_transform.basis.y
-	var desired_f := (point - global_transform.origin)
-	desired_f.y = 0
-	var angle := forward.angle_to(desired_f)
+	dir.y = 0
+	var angle := forward.angle_to(dir)
 	if abs(angle) > 0.01:
-		var axis := forward.cross(desired_f).normalized()
+		var axis := forward.cross(dir).normalized()
 		if !axis.normalized():
 			axis = Vector3.UP
 		var rot := sign(angle)*min(abs(angle), delta*10.0)
