38299ce8d035abb564ec3c30a0483ba118341438
needleful <needleful@outlook.com>
2023-03-28 01:40:13 -0700

    Single thing works

--- entities/env/soft_body.gd
+++ entities/env/soft_body.gd
@@ -3,23 +3,25 @@ extends SoftBody
 var queued_pause := false
 
 func _enter_tree():
-	if !physics_enabled:
-		physics_enabled = true
-		queued_pause = true
-		call_deferred("disable_on_delay")
-	
+	physics_enabled = true
+	queued_pause = true
+	call_deferred("disable_on_delay")
+
 func disable_on_delay():
 	yield(VisualServer, "frame_post_draw")
-	yield(get_tree().create_timer(0.05 + 0.1*randf()), "timeout")
+	yield(get_tree().create_timer(0.1 + 0.1*randf()), "timeout")
 	if !queued_pause:
 		return
+	queued_pause = false
 	physics_enabled = false
 
 func process_player_distance(player_origin):
+	if queued_pause:
+		return
 	var l = (player_origin - get_parent().global_transform.origin).length_squared()
 	visible = l <= 20000
 	physics_enabled = l <= 100
 
 func _exit_tree():
-	#physics_enabled = false
+	physics_enabled = false
 	queued_pause = false
