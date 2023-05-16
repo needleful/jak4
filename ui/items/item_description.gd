04dd7027744adf1701d91d9b688b74367d3993e6
needleful <needleful@outlook.com>
2023-05-15 19:25:57 -0700

    Added tags to items

--- ui/items/item_description.gd
+++ ui/items/item_description.gd
@@ -13,6 +13,7 @@ export(Texture) var custom_icon
 export(PackedScene) var preview_3d
 export(String) var full_name
 export(String, MULTILINE) var description
+export(Array, String) var tags
 # key: item id
 # value: visual name
 export(Dictionary) var extra_items
