d008f84dfc217da3349f5001e6fbbc11b3e922b6
needleful <needleful@outlook.com>
2023-05-31 20:55:45 -0700

    Stamina bar

--- ui/fancy/segmented_bar.gd
+++ ui/fancy/segmented_bar.gd
@@ -8,6 +8,7 @@ export(float) var segment_interval: float
 export(float) var speed := 20.0
 export(float) var acceleration := 2.0
 export(float) var deceleration := 1.0
+export(bool) var reversed := false
 
 onready var segment_template := $segment
 onready var n := get_node(tracking_node)
@@ -23,6 +24,8 @@ func _ready():
 	remove_child(segment_template)
 
 func _process(delta):
+	if visible and !is_visible_in_tree():
+		return
 	var s:int = n.get(segment_property)
 	var change_size := s != segments.size()
 	
@@ -30,7 +33,10 @@ func _process(delta):
 		var seg = segment_template.duplicate()
 		seg.material = segment_template.material.duplicate()
 		seg.material["shader_param/value"] = 0
-		add_child(seg)
+		if reversed and get_child_count():
+			add_child_below_node(get_child(get_child_count() - 1), seg)
+		else:
+			add_child(seg)
 		segments.append(seg)
 	
 	var value:float = n.get(value_property)
@@ -46,12 +52,14 @@ func _process(delta):
 	amount = move_toward(amount, v, abs(vel))
 	var i := 0
 	var removed := 0
+	var seglen := segments.size() - 1
 	for segment in segments:
-		var segment_value := amount - i
+		var index:int = i if !reversed else seglen - i
+		var segment_value:float = amount - index
 		segment.material["shader_param/value"] = segment_value
-		if s <= i and segment_value <= 0:
+		if s <= index and segment_value <= 0:
 			segment.queue_free()
 			removed += 1
 		i += 1
 	for _r in removed:
-		var _x = segments.pop_back()
+		var _x = segments.pop_back() if !reversed else segments.pop_front()
