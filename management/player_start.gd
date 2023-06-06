ed8b80e3128ebd425be9a2d883b998f38b13e789
needleful <needleful@outlook.com>
2023-06-06 10:53:52 -0700

    Jackie's notes

--- management/player_start.gd
+++ management/player_start.gd
@@ -7,7 +7,10 @@ func _ready():
 			"My landing point on this excursion. Not much to look at aside from the Medium.")
 		Global.remember("mum")
 		Global.add_note(
-			"I suppose if I'd been born out here, I'd have been like Mother. Austere. Stoic. That's what they tell me about her, at least.", ["mum"])
+			"I suppose if I'd been born out here, I'd have been like Mother. Austere. Stoic. That's what Father about her, at least.", ["mum"])
+		Global.remember("jackie")
+		Global.add_note(
+			"This is the journal of Jacqueline Southcott. Please do not read beyond this point.\nIf found, please return by post to\n\t1292 North Graham Road\n\tSecond Floor, Apartment 22\n\tDockson.", ["jackie"])
 		var p = get_player()
 		if p:
 			p.teleport_to(global_transform)
