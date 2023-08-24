9b25d1412050aec7b5e73ccf6662625c0bb57295
needleful <needleful@outlook.com>
2023-08-23 20:07:00 -0700

    Unused file

--- areas/chunks/activation.gd
+++
@@ -1,7 +0,0 @@
-extends Spatial
-
-func _ready():
-	for c in get_children():
-		var door := $"../door"
-		var _x = c.connect("activated", door, "_on_activated")
-		_x = c.connect("deactivated", door, "_on_deactivated")
