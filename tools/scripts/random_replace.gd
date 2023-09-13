d13aafe4257e3e3df08d7c35b6ee75cbbb05199b
needleful <needleful@outlook.com>
2023-09-12 19:33:51 -0700

    Added muzna prefabs, about to replace boxcars

---
+++ tools/scripts/random_replace.gd
@@ -0,0 +1,38 @@
+tool
+extends EditorScript
+
+var target_group := "cargo_box"
+var resource_folder := "res://entities/env/train_set/muzna_city"
+
+var list: Array
+
+func _run():
+	list = all_scenes_in_path(resource_folder)
+	for l in list:
+		if l is PackedScene:
+			print(l.resource_path)
+	
+	var scn := get_scene()
+	for node in scn.get_tree().get_nodes_in_group(target_group):
+		var parent :Node = node.get_parent()
+		var new_node = get_from_list().instance()
+		parent.add_child(new_node)
+		new_node.owner = scn
+		if node is Spatial:
+			new_node.transform = node.transform
+			node.queue_free()
+
+func get_from_list() -> PackedScene:
+	return list[int(rand_range(1, list.size())) - 1]
+
+func all_scenes_in_path(path: String) -> Array:
+	var result := []
+	var dir := Directory.new()
+	if dir.open(path) == OK:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if !dir.current_is_dir() and file_name.ends_with(".tscn"):
+				result.append(ResourceLoader.load(resource_folder + "/" + file_name))
+			file_name = dir.get_next()
+	return result
