ece280aec65ee290461a39c47b2a15b62cd9e4ba
needleful <needleful@outlook.com>
2023-10-06 23:13:36 -0700

    Fix warnings and print when loading/saving the monitor fails

--- management/default_monitor.gd
+++ management/default_monitor.gd
@@ -5,12 +5,18 @@ const path := "user://default_monitor"
 func _enter_tree():
 	var f := File.new()
 	if f.file_exists(path):
-		f.open(path, File.READ)
+		var r = f.open(path, File.READ)
+		if r != OK:
+			print_debug("Failed to load default monitor")
+			return
 		var i :int = f.get_8()
 		if i > 0:
 			OS.current_screen = i
 
 func _exit_tree():
 	var f := File.new()
-	f.open(path, File.WRITE)
+	var r = f.open(path, File.WRITE)
+	if r != OK:
+		print_debug("Failed to save default monitor")
+		return
 	f.store_8(OS.current_screen)
