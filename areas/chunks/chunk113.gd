30348699b4852fd41bd287aad10656cbc3722b47
needleful <needleful@outlook.com>
2023-11-05 20:29:38 -0800

    Added dark ambient lighting

--- areas/chunks/chunk113.gd
+++ areas/chunks/chunk113.gd
@@ -3,6 +3,7 @@ extends Chunk
 
 export(String) var reflection_group
 export(Color) var deactivated_color
+export(Color) var deactivated_dark_color
 export(NodePath) var active_lights
 export(NodePath) var inactive_lights
 
@@ -11,6 +12,7 @@ func _on_power_deactivated():
 		var _x = Global.add_stat("capacitor_113")
 		_x = Global.abolish_notes(["the_tree_tower", "village_tower"])
 	get_tree().call_group(reflection_group, "set", "interior_ambient_color", deactivated_color)
+	get_tree().call_group(reflection_group, "set", "interior_dark_ambient_color", deactivated_dark_color)
 	for c in get_node(active_lights).get_children():
 		if c is Light:
 			c.hide()
