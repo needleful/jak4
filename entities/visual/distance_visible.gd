26d9e9ccef17f084d09748f4dca06e93bfc952d3
needleful <needleful@outlook.com>
2023-11-09 19:50:56 -0800

    Update crops to be hidden from a distance

--- entities/visual/distance_visible.gd
+++ entities/visual/distance_visible.gd
@@ -1,7 +1,14 @@
 extends Spatial
 
+enum Mode {
+	NodeAndChildren,
+	InstanceLayer
+}
+
 export(float, 0, 1000) var distance := 30.0
+export(Mode) var mode := Mode.NodeAndChildren
 onready var dist_sq = distance*distance
+onready var starting_layers = 0 if !("layers" in self) else self.layers
 
 func _ready():
 	add_to_group("distance_activated")
@@ -10,4 +17,8 @@ func process_player_distance(player_pos):
 	if !is_inside_tree():
 		return
 	var r2 := Global.render_distance*Global.render_distance
-	visible = (player_pos - global_transform.origin).length_squared() <= dist_sq*r2
+	var should_show: bool = (player_pos - global_transform.origin).length_squared() <= dist_sq*r2
+	if mode == Mode.NodeAndChildren:
+		visible = should_show
+	elif mode == Mode.InstanceLayer and "layers" in self:
+		self.layers = starting_layers if should_show else 0
