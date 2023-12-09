0a18bcbe375e465d73b236a68e43984e0e4c7347
needleful <needleful@outlook.com>
2023-12-08 21:06:19 -0800

    Redrew map with nice border

--- areas/test/demo_fence.gd
+++ areas/test/demo_fence.gd
@@ -1,10 +1,17 @@
+tool
 extends MeshInstance
 
 var mat: ShaderMaterial
 
 func _ready():
+	if Engine.editor_hint:
+		set_process(false)
+	else:
+		material_override = null
 	mat = get_surface_material(0)
-	mat.set_shader_param("debug", false)
 
 func _process(_delta):
+	if Engine.editor_hint:
+		print_debug("I set this to false, fuck off Godot")
+		set_process(false)
 	mat.set_shader_param("world_player", Global.get_player().global_transform.origin)
