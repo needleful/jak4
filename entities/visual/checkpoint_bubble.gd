b5e629095ff337f0fb8afe646cf8034fb3ba9a76
needleful <needleful@outlook.com>
2023-12-10 19:06:08 -0800

    The bubble only shows for new checkpoints

--- entities/visual/checkpoint_bubble.gd
+++ entities/visual/checkpoint_bubble.gd
@@ -4,5 +4,9 @@ func _ready():
 	var _x = get_parent().connect("saved", self, "_on_saved")
 
 func _on_saved():
+	if Global.temp_stat("checkpoint") and Global.temp_stat("checkpoint") == self:
+		return
+	else:
+		Global.set_temp_stat("checkpoint", self)
 	$anim.stop()
 	$anim.play("save")
